4dd069866ca11fe0f04a77f80388f6750c6bbb3b
william <liuhuiping@sakura-wave.com>
2021-08-19 16:14:05 +0800

    add random shuffle strsplit and some function

--- util.h
+++ util.h
@@ -1,4 +1,6 @@
 #include <chrono>
+#include <vector>
+#include <string>
 
 
 /*  时间相关函数 */
@@ -7,3 +9,10 @@ time_t now_ms();
 
 /*  随机函数 */
 uint32_t random(size_t n);
+
+/* 高德纳洗牌法 */
+void shuffle(std::vector<uint32_t> rndVec);
+
+
+/* 字符串解析函数 */
+std::vector<std::string> StrSplit(const std::string& src, const std::string& sep);
